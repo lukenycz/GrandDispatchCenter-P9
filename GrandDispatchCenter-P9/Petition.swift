41821e48f3947df64034e63f296ad85a6b542ee6
Łukasz Nycz <lnycz96@gmail.com>
2021-07-13 11:51:49 +0200

    Preparing Project to GDC

---
+++ GrandDispatchCenter-P9/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  GrandDispatchCenter-P9
+//
+//  Created by Łukasz Nycz on 13/07/2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ GrandDispatchCenter-P9/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  GrandDispatchCenter-P9
+//
+//  Created by Łukasz Nycz on 13/07/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results:[Petition]
+}
